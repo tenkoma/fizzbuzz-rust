e1da9ad447d2a87da61125959536d2b6da98555e
Koji Tanaka <tenkoma@gmail.com>
2024-02-11 19:09:56 +0900

    feat: 仮実装

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+fn fizzbuzz(num: u64) -> String {
+    String::from("1")
+}
+
 #[cfg(test)]
 mod tests {
-
+    use crate::fizzbuzz;
     /// 1を渡すと文字列'1'を返す
     #[test]
     fn passing_one_returns_the_string_one() {
