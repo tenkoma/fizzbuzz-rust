dd3486d650406a193e1c752be5508db79b46626b
Koji Tanaka <tenkoma@gmail.com>
2024-02-11 19:10:19 +0900

    test: 2を渡すと文字列'2'を返す

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,10 @@ mod tests {
     fn passing_one_returns_the_string_one() {
         assert_eq!(fizzbuzz(1), String::from("1"));
     }
+
+    /// 2を渡すと文字列'2'を返す
+    #[test]
+    fn passing_two_returns_the_string_two() {
+        assert_eq!(fizzbuzz(2), String::from("2"));
+    }
 }
